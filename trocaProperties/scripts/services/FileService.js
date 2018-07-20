2a7f6fff231919540623cf03ddbfcd54937cde52
natan <natan.spricigo@camtwo.com.br>
2018-07-20 17:54:33 -0300

    master [melhorado arquivo de leitura de arquivos]

--- trocaProperties/scripts/services/FileService.js
+++ trocaProperties/scripts/services/FileService.js
@@ -1,55 +1,58 @@
 fs = require('fs');
 
-module.exports.fileService = function() {
-	return {
-		isFile: function(path, onExist, onNotFound) {
-			fs.stat(path, function(err, fileStat) {
-				if (err) {
-					console.log('ERR: ');
-					if (err.code == 'ENOENT') {
-						console.log('File %s Does not exist.', path);
-					}
-					onNotFound(err, path);
-				} else {
-					onExist(fileStat, path);
-				}
-			});
-		},
-		read: function(path, onRead) {
-			fs.readFile(path, 'utf8', function(err, fileContents) {
-				if (err) throw new err;
-				if (typeof onRead == 'function') {
-					onRead(fileContents, err);
-				} else {
-					console.log(fileContents);
-				}
-			});
+module.exports.fileService = function () {
 
-		},
-		readFiles: function(dirname, onFileContent, onError) {
-			fs.readdir(dirname, function(err, filenames) {
-				if (err) {
-					onError(err);
-					return;
-				}
-				filenames.forEach(function(filename) {
-					fs.readFile(dirname + filename, 'utf-8', function(err, content) {
-						if (err) {
-							onError(err);
-							return;
-						}
-						onFileContent(filename, content);
-					});
-				});
-			});
-		},
-		write: function(path, data, onComplete){
-			fs.writeFile(path, data, {encoding:'utf8'}, function (err) {
-			    onComplete(err);
-			});
-		},
-		clear: function(path, onComplete){
-			this.write(path,"",onComplete);
-		}
-	}
+    return {
+        encoding: 'utf8',
+        isFile: function (path, onExist, onNotFound) {
+            fs.stat(path, function (err, fileStat) {
+                if (err) {
+                    console.log('ERR: ');
+                    if (err.code == 'ENOENT') {
+                        console.log('File %s Does not exist.', path);
+                    }
+                    onNotFound(err, path);
+                } else {
+                    onExist(fileStat, path);
+                }
+            });
+        },
+        read: function (path, onRead) {
+            fs.readFile(path, this.encoding, function (err, fileContents) {
+                if (err) throw err;
+                if (typeof onRead == 'function') {
+                    onRead(fileContents, err);
+                } else {
+                    console.log(fileContents);
+                }
+            });
+        },
+        readFiles: function (dirname, onFileContent, onError) {
+            fs.readdir(dirname, function (err, filenames) {
+                if (err) {
+                    onError(err);
+                    return;
+                }
+                filenames.forEach(function (filename) {
+                    fs.readFile(dirname + filename, this.encoding, function (err, content) {
+                        if (err) {
+                            onError(err);
+                            return;
+                        }
+                        onFileContent(filename, content);
+                    });
+                });
+            });
+        },
+        write: function (path, data, onComplete) {
+            fs.writeFile(path, data, {
+                encoding: this.encoding
+            }, function (err) {
+                onComplete(err);
+            });
+        },
+        clear: function (path, onComplete) {
+            this.write(path, "", onComplete);
+        }
+    }
 }();
