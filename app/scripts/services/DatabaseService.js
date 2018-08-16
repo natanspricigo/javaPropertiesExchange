7d9f484a7d158063a2bba3112b519fea9d4e29da
natan <natan.spricigo@camtwo.com.br>
2018-08-16 10:16:30 -0300

    master [melhorias em layout e add botão re refresh]

--- app/scripts/services/DatabaseService.js
+++ app/scripts/services/DatabaseService.js
@@ -4,71 +4,89 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-class DatabaseMananger{
+class DatabaseMananger {
 
-	constructor(){
-		this.tableNames={PREFERENCIAS:"preferencias", HISTORICO: "historico", MAPA:"mapa"};
-		db.defaults({ preferencias: [], count: 0 }).write();
-		db.defaults({ historico: []}).write();
-		db.defaults({ mapa: []}).write();
+	constructor() {
+		this.tableNames = {
+			PREFERENCIAS: "preferencias",
+			HISTORICO: "historico",
+			MAPA: "mapa"
+		};
+		db.defaults({
+			preferencias: [],
+			count: 0
+		}).write();
+		db.defaults({
+			historico: []
+		}).write();
+		db.defaults({
+			mapa: []
+		}).write();
 		this.nativeDb = db;
 	}
-	
-	__getTable(table){
+
+	__getTable(table) {
 		if (db.has(table).value()) {
 			return db.get(table);
 		};
 		return undefined;
 	}
 
-	__findMaxIDToTable(table){
+	__findMaxIDToTable(table) {
 		var cod = 0;
-		this.getAll(table).forEach(function(index) {
-			if (index.id > cod) {
-				cod = index.id;
-			};
-		});
+		var all = this.getAll(table);
+		if (all) {
+			this.getAll(table).forEach(function(index) {
+				if (index.id > cod) {
+					cod = index.id;
+				};
+			});
+		};
 		return cod || 0;
 	}
 
-	insert(table, data){
+	insert(table, data) {
 		if (data && (data.id == undefined || data.id == "" || data.id == null || isNaN(data.id))) {
 			data.id = this.__findMaxIDToTable(table) + 1;
-		}else{
+		} else {
 			this.update(table, data);
 		}
 
-		data.id=Number(data.id);
+		data.id = Number(data.id);
 		var result = this.__getTable(table).push(data).write();
 		db.update('count', n => n + 1).write();
 		return result;
 	}
 
-	get(table, id){
-		var retorno = this.__getTable(table).find({ "id": Number(id) }).value();
+	get(table, id) {
+		var retorno = this.__getTable(table).find({
+			"id": Number(id)
+		}).value();
 		if (retorno) {
 			return retorno;
 		}
 	}
-	
-	getAll(table){
+
+	getAll(table) {
 		return db.get(table).value()
 	}
 
-	update(table, newData){
-		newData.id=Number(newData.id);
+	update(table, newData) {
+		newData.id = Number(newData.id);
 		return this.__getTable(table)
-		  .find({ id: newData.id})
-		  .assign(newData)
-		  .write();
+			.find({
+				id: newData.id
+			})
+			.assign(newData)
+			.write();
 	}
 
-	remove(table, obj){
+	remove(table, obj) {
 		if (obj) {
-			console.warn("Removendo codigo %s da tabela %s", (obj?obj.id:"???????"), table);
+			console.warn("Removendo codigo %s da tabela %s", (obj ? obj.id : "???????"), table);
 			return this.__getTable(table)
-			  .remove(obj)
-			  .write();
+				.remove(obj)
+				.write();
 		}
 	}
 }
